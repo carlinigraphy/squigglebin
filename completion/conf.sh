84a0d12bdaae575e7eb7b2d33b20fb31051ac203
Marcus Aurelius <github-thenwhencecomethevil@a.aurelius.org>
2023-10-09 00:14:38 -0700

    refactor: allow for path expansion, treeshaking
    Removed a lot of unnecessary nonsense. All of the argument parsing
    functions could be rolled into the main command function. They weren't
    serving sufficient purpose to be necessary. Don't think I'd ever have
    more arguments down the line I'd need to parse.
    
    Moved path expansion to only when called, rather than DB load. Moved DB
    validation where it feels more appropritate. Honestly probably missing
    some places where it should be though.

--- completion/conf.sh
+++ completion/conf.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 # vim: ft=bash tw=80
 
-declare -g DATA_FILE="${XDG_DATA_HOME:-${HOME}/.local/share/twce/conf}"/data
-declare -g DATA_DIR="${DATA_FILE%/*}"
+function __conf {
+   local DATA_FILE="${XDG_DATA_HOME:-${HOME}/.local/share/twce/conf}"/data
 
-function _conf_comp_main {
-   local -a opts
+   local curr="${COMP_WORDS[$COMP_CWORD]}"
+   local prev="${COMP_WORDS[$COMP_CWORD-1]}"
 
-   while IFS=$'\n' read -r line ; do
-      # Comments.
-      if [[ $line =~ ^[[:space:]]*# ]] ; then
-         continue
-      fi
+   local -a nicks=()
+   local -a cmds=(
+      --help
+      help
+      set
+      cd
+      sort
+      clean
+      db
+      list
+      edit
+   )
 
-      # Nicknames.
-      if [[ $line =~ ^([^[:space:]]+) ]] ; then
-         opts+=( "${BASH_REMATCH[1]}" )
-      fi
-   done < "$DATA_FILE"
+   if (( COMP_CWORD == 1 )) ; then
+      COMPREPLY=( $(compgen -W "${cmds[*]}" -- "${curr}") )
+      return
+   fi
 
-   COMPREPLY=( $(compgen -W "${opts[*]}" -- "${COMP_WORDS[$COMP_CWORD]}" ) )
+   mapfile -t possible < <(
+      compgen -W "${cmds[*]}" -- "$prev"
+   )
+
+   if (( ${#possible[@]} == 1 )) ; then
+      prev="${possible}" 
+   fi
+
+   if [[ $prev == help ]] ; then
+      unset 'cmds[1]' 'cmds[0]'
+      COMPREPLY=( $(compgen -W "${cmds[*]}" -- "${curr}") )
+   elif [[ $prev =~ (set|cd|edit|,|\.) ]] ; then
+      local line
+      while IFS=$'\n' read -r line ; do
+         [[ $line =~ ^[[:space:]]*# ]]   && continue
+         [[ $line =~ ^([^[:space:]]+) ]] && nicks+=( "${BASH_REMATCH[1]}" )
+      done < "$DATA_FILE"
+      COMPREPLY=( $(compgen -W "${nicks[*]}" -- "${curr}") )
+   fi
 }
 
-complete -F _conf_comp_main conf
+complete -F __conf conf
