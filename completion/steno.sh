0aabab79a6bfbe1c944b16b89199f76ea8bc6fcc
marcus <marcus@carlini.com>
2024-10-16 23:39:09 -0700

    fix: steno

--- completion/steno.sh
+++ completion/steno.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-# shellcheck disable=SC2207
+# shellcheck disable=SC2207,SC2088
 
 function __steno {
    local curr="${COMP_WORDS[$COMP_CWORD]}"
    local prev="${COMP_WORDS[$COMP_CWORD-1]}"
 
-   local -a cmds=( cd print )
+   local -a cmds=(
+      cd
+      edit
+      print
+   )
+
    if (( COMP_CWORD == 1 )) ; then
       COMPREPLY=( $(compgen -W "${cmds[*]}" -- "$curr") )
       return
    fi
 
-   if [[ $prev == 'print' ]] ; then
+   if [[ $prev == 'edit'  ]] ||
+      [[ $prev == 'print' ]]
+   then
       if [[ ! "$curr" ]] ; then
          compopt -o nospace
-         COMPREPLY=( ~/hg/crah/lesson_notes/ )
+         COMPREPLY=( '~/hg/steno/lesson_notes/' )
       else
          compopt -o default ; return
       fi
